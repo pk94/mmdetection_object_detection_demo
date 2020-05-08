7e20e4dea2a633ca95ec2e62b69bdda5fae6a558
pk94 <kowaleczko.p@gmail.com>
2020-05-08 19:16:31 +0200

    Updated dataset

--- misc/generate_annotations.py
+++ misc/generate_annotations.py
@@ -1,6 +1,7 @@
 from os import listdir
 from os.path import isfile, join
 import xml.etree.ElementTree as ET
+from PIL import Image
 
 def train_test_split(files, files_path, test=0.1):
     split = int(len(files) * test)
@@ -15,14 +16,22 @@ def train_test_split(files, files_path, test=0.1):
             f1.write(f'{line[:-4]}\n')
     f1.close()
 
+def png_to_jpg(path):
+    files = [f for f in listdir(path) if isfile(join(path, f))]
+    for file in files:
+        im1 = Image.open(f'{path}\\{file}')
+        im1.save(f'{path}2\{file[:-3]}jpg')
 
-dataset_path = r'C:\Pawel\Datasets\planes\Images'
-ann_save_path = r'C:\Pawel\Datasets\planes\Annotations\\'
-train_test_path = r'C:\Pawel\Datasets\planes\ImagesSets'
+
+dataset_path = r'..\data\VOC2007\JPEGImages'
+ann_save_path = r'..\data\VOC2007\Annotations\\'
+train_test_path = r'..\data\VOC2007\ImageSets'
 
 files = [f for f in listdir(dataset_path) if isfile(join(dataset_path, f))]
 tree = ET.parse('template.xml')
 root = tree.getroot()
+
+# png_to_jpg(dataset_path)
 
 for file in files:
     for filename in root.iter('filename'):
